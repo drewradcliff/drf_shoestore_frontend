131e4714581b90fd5825c8093d6322f893066759
Drew Radcliff <sirampr@gmail.com>
2020-09-25 16:19:23 -0400

    Fix warning

--- src/App.js
+++ src/App.js
@@ -6,22 +6,22 @@ function App() {
     fetch("http://127.0.0.1:8000/api/shoe/")
       .then((res) => res.json())
       .then((data) => setShoes(data));
-  });
+  }, []);
 
   return (
     <>
-      {shoes.map((shoe) => (
-        <>
+      {shoes.map((shoe, i) => (
+        <div key={i}>
           <h3>{shoe.brand_name}</h3>
           <ul>
-            <li>{shoe.size}</li>
-            <li>{shoe.manufacturer}</li>
-            <li>{shoe.color}</li>
-            <li>{shoe.material}</li>
-            <li>{shoe.shoe_type}</li>
-            <li>{shoe.fasten_type}</li>
+            <li>Size: {shoe.size}</li>
+            <li>Manufacturer: {shoe.manufacturer}</li>
+            <li>Color: {shoe.color}</li>
+            <li>Material: {shoe.material}</li>
+            <li>Type: {shoe.shoe_type}</li>
+            <li>Fasten type: {shoe.fasten_type}</li>
           </ul>
-        </>
+        </div>
       ))}
     </>
   );
